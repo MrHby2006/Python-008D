e99a15ea7b8a7a424127074580b01a89840999ad
Harol <harolseptimo2019@gmail.com>
2025-07-02 13:27:20 -0400

    Orden de archivos

--- Main_Turistas.py
+++
@@ -1,3 +0,0 @@
-from Turistas import menu, turistas_por_pais, turistas_por_mes, eliminar_turista
-
-menu()
---
+++ Trabajos_y_Ejercicios/Main_Turistas.py
@@ -0,0 +1,3 @@
+from Turistas import menu, turistas_por_pais, turistas_por_mes, eliminar_turista
+
+menu()
